a3ef2d293536c0cc7a4ae51311183e45f75d1d0b
April Copes <antdevelopment1@gmail.com>
2018-10-04 22:14:29 -0400

    returns the day of the week

---
+++ day_of_the_week.js
@@ -0,0 +1,4 @@
+// Get the day of the week by passing the value.   
+var user_input = parseInt(prompt("Enter a number to represent the day of the week? "));
+var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+console.log(`Today is ${week[user_input]}.`);
